b8495789de18897c02fab922b2af6f5479f4fe5e
Jason Yee <jasonyee1776@gmail.com>
2018-08-01 11:20:42 -0700

    Analog clock made using CSS and JavaScript

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
+
+function setDate() {
+    const date = new Date;
+    const second = date.getSeconds();
+    const secondDegree = ((second / 60) * 360);
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+    const minute = date.getMinutes();
+    const minuteDegree = ((minute / 60) * 360);
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+    const hour = date.getHours();
+    const hourDegree = ((hour / 12) * 360);
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+}
+
+
+setInterval(() => {
+    setDate();
+}, 1000);
+
+
+
+
+// secondTick();
+// minuteTick();
+// hourTick();
